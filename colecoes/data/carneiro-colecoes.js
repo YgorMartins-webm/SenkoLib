88bde6fbcb38bcdca6fda96c40df3f90d4ccb45e
YgorMartins-webm <fygormartins@gmail.com>
2026-04-17 13:24:39 -0300

    [SenkoLib] create collection: carneiro-colecoes

---
+++ colecoes/data/carneiro-colecoes.js
@@ -0,0 +1,15 @@
+// @ts-nocheck
+/* ═══════════════════════════════════════════════════════════════════════
+   colecoes/data/carneiro-colecoes.js — Coleção: Carneiro Coleções
+
+   ATENÇÃO: Arquivo gerado pelo módulo GitHub do SenkoLib.
+   NÃO edite manualmente em produção.
+═══════════════════════════════════════════════════════════════════════ */
+ColLib.register({
+  slug:  'carneiro-colecoes',
+  name:  'Carneiro Coleções',
+  group: 'ygor',
+  tags:  ['Responsivo'],
+  layouts: [
+  ]
+});
