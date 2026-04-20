ca1ceb60dce03b0d95d26300fd82904857f8173b
YgorMartins-webm <fygormartins@gmail.com>
2026-04-20 10:52:07 -0300

    [SenkoLib] create collection: colecao-joel

---
+++ colecoes/data/colecao-joel.js
@@ -0,0 +1,15 @@
+// @ts-nocheck
+/* ═══════════════════════════════════════════════════════════════════════
+   colecoes/data/colecao-joel.js — Coleção: Coleção Joel
+
+   ATENÇÃO: Arquivo gerado pelo módulo GitHub do SenkoLib.
+   NÃO edite manualmente em produção.
+═══════════════════════════════════════════════════════════════════════ */
+ColLib.register({
+  slug:  'colecao-joel',
+  name:  'Coleção Joel',
+  group: 'joel',
+  tags:  ['Responsivo'],
+  layouts: [
+  ]
+});
