24c2ef790f3a6eec298a405229995e5e029070d8
YgorMartins-webm <fygormartins@gmail.com>
2026-04-23 08:14:53 -0300

    [SenkoLib] create collection: colecao-pedro

---
+++ colecoes/data/colecao-pedro.js
@@ -0,0 +1,15 @@
+// @ts-nocheck
+/* ═══════════════════════════════════════════════════════════════════════
+   colecoes/data/colecao-pedro.js — Coleção: Coleção Pedro
+
+   ATENÇÃO: Arquivo gerado pelo módulo GitHub do SenkoLib.
+   NÃO edite manualmente em produção.
+═══════════════════════════════════════════════════════════════════════ */
+ColLib.register({
+  slug:  'colecao-pedro',
+  name:  'Coleção Pedro',
+  group: 'pedro',
+  tags:  [],
+  layouts: [
+  ]
+});
