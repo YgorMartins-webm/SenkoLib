468d664ea58bd3288a3cf6c5c8295941dcca16aa
YgorMartins-webm <fygormartins@gmail.com>
2026-05-21 15:08:55 -0300

    [SenkoLib] edit layout: section-2

--- layouts/layouts001.js
+++ layouts/layouts001.js
@@ -188,7 +188,7 @@ SenkoLib.register([
   {
     id: 'section-2',
     name: 'Section-2 (Header Vídeo)',
-    tags: ['Section-2', 'Section 2', '2', 'vídeo', 'header vídeo'],
+    tags: ['Section-2', 'Section 2', '2', 'vídeo', 'header vídeo', 'responsivo', 'acessível'],
     html: ` <article style="width: 100%;" aria-labelledby="product-main-title"> <!-- fecha esse article lá no final do código, depois de todo conteúdo, e antes do fechamento da div.lp-container -->
     <section class="video-header" aria-label="Cabeçalho do produto">
   <header class="video-header__banner">
