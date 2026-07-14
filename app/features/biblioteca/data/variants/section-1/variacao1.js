25b0310b009986ea2062429bd0182421bccc8e51
YgorMartins-webm <fygormartins@gmail.com>
2026-07-14 10:37:15 -0300

    [SenkoLib] edit variant file: section-1/variacao1

--- app/features/biblioteca/data/variants/section-1/variacao1.js
+++ app/features/biblioteca/data/variants/section-1/variacao1.js
@@ -93,7 +93,7 @@ SenkoLib.registerVariantFile("section-1",
     display: flex;
     flex-direction: column;
     align-items: center;
-    padding: calc(var(--badge-size) / 2 + 1.8rem) 0.3rem 2.5rem;
+    padding: calc(var(--badge-size) / 2 + 1.8rem) 0.3rem 0.5rem;
     text-align: center;
   }
 
@@ -139,6 +139,8 @@ SenkoLib.registerVariantFile("section-1",
     .product-header__subtitle-2 {
       text-align: justify;
       font-size: clamp(0.9rem, 2vw, 1.1rem);
+      max-width: 95%;
+      margin: 1.25rem auto 0;
     }
   }
 </style>`
