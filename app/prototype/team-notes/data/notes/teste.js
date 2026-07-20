943b41eaeff0a5aeeb35b444979cf359d48a5784
YgorMartins-webm <fygormartins@gmail.com>
2026-07-20 13:19:34 -0300

    [Team Notes] save note: teste

---
+++ app/prototype/team-notes/data/notes/teste.js
@@ -0,0 +1,11 @@
+window.SenkoTeamNotesData.registerNote({
+  "id": "teste",
+  "title": "Teste",
+  "type": "prompt",
+  "tags": [
+    "awd"
+  ],
+  "content": "wdaw",
+  "createdAt": "2026-07-20T16:19:33.095Z",
+  "updatedAt": "2026-07-20T16:19:33.095Z"
+});
