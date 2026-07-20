f82ec76556bc3c7e351fcea1ef8d5093304e9b82
YgorMartins-webm <fygormartins@gmail.com>
2026-07-20 13:19:35 -0300

    [Team Notes] update manifest

--- app/prototype/team-notes/data/manifest.js
+++ app/prototype/team-notes/data/manifest.js
@@ -2,6 +2,16 @@
 window.SenkoTeamNotesManifest = {
   "notes": [
     {
+      "id": "teste",
+      "title": "Teste",
+      "type": "prompt",
+      "tags": [
+        "awd"
+      ],
+      "file": "notes/teste.js",
+      "updatedAt": "2026-07-20T16:19:33.095Z"
+    },
+    {
       "id": "padronizacao-html",
       "title": "PADRONIZAÇÃO HTML",
       "type": "prompt",
@@ -10,7 +20,7 @@ window.SenkoTeamNotesManifest = {
         "layouts"
       ],
       "file": "notes/padronizacao-html.js",
-      "updatedAt": "2026-07-20T16:10:19.953Z"
+      "updatedAt": "2026-07-20T16:10:34.064Z"
     }
   ]
 };
