b7b2cb87a044678bd0b227f90d3df741ff1b5a10
YgorMartins-webm <fygormartins@gmail.com>
2026-08-10 09:58:20 -0300

    [SenkoLib] register layout file: section-18

--- app/features/biblioteca/data/manifest.js
+++ app/features/biblioteca/data/manifest.js
@@ -160,6 +160,11 @@ window.SenkoBibliotecaManifest = {
       "file": "layouts/section-28.js",
       "id": "section-28",
       "name": "Section-28"
+    },
+    {
+      "file": "layouts/section-18.js",
+      "id": "section-18",
+      "name": "Section-18"
     }
   ],
   "variants": [
